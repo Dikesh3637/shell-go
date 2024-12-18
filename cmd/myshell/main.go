33cc28d8f82ea9616796da745086fb1429595674
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-12-18 21:04:33 +0530

    2#CZ2

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,20 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	//empty set using a map
 	set := make(map[string]struct{})
-
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
 
 		if _, ok := set[input]; !ok {
-			fmt.Printf("%s: command not found", input)
+			fmt.Printf("%s: command not found\n", input)
 		}
 	}
 }
