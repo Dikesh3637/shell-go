8603b0623177cf9c80024753685b4e4b031f9a19
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-12-18 19:47:42 +0530

    stage 008

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,13 +6,10 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
+	//Uncomment this block to pass the first stage
+	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
+	//Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
